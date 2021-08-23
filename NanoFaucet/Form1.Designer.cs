707f665c9200b9150f4bf41433ae4697348f1984
Tran Quoc Dai <mr.tranquocdai@gmail.com>
2021-08-23 23:48:08 +0700

    Add files via upload

---
+++ NanoFaucet/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace NanoFaucet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BTN_Start = new System.Windows.Forms.Button();
+            this.TXT_wallet = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TXT_Logs = new System.Windows.Forms.TextBox();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.LBL_Time = new System.Windows.Forms.Label();
+            this.LinkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // BTN_Start
+            // 
+            this.BTN_Start.Location = new System.Drawing.Point(401, 27);
+            this.BTN_Start.Name = "BTN_Start";
+            this.BTN_Start.Size = new System.Drawing.Size(89, 31);
+            this.BTN_Start.TabIndex = 0;
+            this.BTN_Start.Text = "START";
+            this.BTN_Start.UseVisualStyleBackColor = true;
+            this.BTN_Start.Click += new System.EventHandler(this.BTN_Start_Click);
+            // 
+            // TXT_wallet
+            // 
+            this.TXT_wallet.Location = new System.Drawing.Point(12, 33);
+            this.TXT_wallet.Name = "TXT_wallet";
+            this.TXT_wallet.Size = new System.Drawing.Size(383, 20);
+            this.TXT_wallet.TabIndex = 1;
+            this.TXT_wallet.Text = "nano_1attz8uksuo86396watngxaxsodzbwtn8563ebma8sc16aefrxqd5zp7jbcw";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "wallet address";
+            // 
+            // TXT_Logs
+            // 
+            this.TXT_Logs.Location = new System.Drawing.Point(12, 100);
+            this.TXT_Logs.Multiline = true;
+            this.TXT_Logs.Name = "TXT_Logs";
+            this.TXT_Logs.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TXT_Logs.Size = new System.Drawing.Size(478, 101);
+            this.TXT_Logs.TabIndex = 3;
+            // 
+            // Timer1
+            // 
+            this.Timer1.Enabled = true;
+            this.Timer1.Interval = 1000;
+            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // LBL_Time
+            // 
+            this.LBL_Time.AutoSize = true;
+            this.LBL_Time.Location = new System.Drawing.Point(12, 70);
+            this.LBL_Time.Name = "LBL_Time";
+            this.LBL_Time.Size = new System.Drawing.Size(30, 13);
+            this.LBL_Time.TabIndex = 4;
+            this.LBL_Time.Text = "Time";
+            // 
+            // LinkLabel1
+            // 
+            this.LinkLabel1.AutoSize = true;
+            this.LinkLabel1.Location = new System.Drawing.Point(273, 70);
+            this.LinkLabel1.Name = "LinkLabel1";
+            this.LinkLabel1.Size = new System.Drawing.Size(122, 13);
+            this.LinkLabel1.TabIndex = 5;
+            this.LinkLabel1.TabStop = true;
+            this.LinkLabel1.Text = "https://tranquocdai.com";
+            this.LinkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 224);
+            this.Controls.Add(this.LinkLabel1);
+            this.Controls.Add(this.LBL_Time);
+            this.Controls.Add(this.TXT_Logs);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TXT_wallet);
+            this.Controls.Add(this.BTN_Start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Free Nano Faucet v1.0 - tranquocdai.com";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN_Start;
+        private System.Windows.Forms.TextBox TXT_wallet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TXT_Logs;
+        private System.Windows.Forms.Timer Timer1;
+        private System.Windows.Forms.Label LBL_Time;
+        private System.Windows.Forms.LinkLabel LinkLabel1;
+    }
+}
+
---
+++ NanoFaucet/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace NanoFaucet
+{
+    public partial class Form1 : Form
+    {
+		bool isStop = false;
+		public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+		private IWebDriver ExcuseBrowser()
+		{
+			ChromeOptions options = new ChromeOptions();
+
+				if (File.Exists("C:\\Program Files\\Google\\Chrome\\Application\\Chrome.exe"))
+				{
+					options.BinaryLocation = "C:\\Program Files\\Google\\Chrome\\Application\\Chrome.exe";
+				}
+				else
+				{
+					if (!File.Exists("C:\\Program Files (x86)\\Google\\Chrome\\Application\\Chrome.exe"))
+					{
+						Invoke((MethodInvoker)delegate
+						{
+							TXT_Logs.Text = "Please Install Chrome Browser First. Visit https://www.google.com/chrome/";
+							Process.Start("https://www.google.com/chrome/");
+						});
+						return null;
+					}
+					options.BinaryLocation = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\Chrome.exe";
+				}
+				options.AddArgument("--disable-gpu");
+				options.AddArgument("--log-level=3");
+				options.AddArgument("--test-type");
+				options.AddArgument("--disable-default-apps");
+				options.AddArgument("--disable-volume-adjust-sound");
+				options.AddArgument("--mute-audio");
+				options.AddArgument("--disable-blink-features=AutomationControlled");
+				options.AddExcludedArgument("enable-automation");
+				options.AddAdditionalCapability("useAutomationExtension", false);
+				options.AddUserProfilePreference("credentials_enable_service", false);
+				options.AddUserProfilePreference("profile.password_manager_enabled", false);
+				ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService();
+				chromeDriverService.HideCommandPromptWindow = true;
+				return new ChromeDriver(chromeDriverService, options, TimeSpan.FromMinutes(3));
+		}
+		private void Action()
+        {
+			if (isStop) { return; }
+
+            try
+            {
+				IWebDriver driver = ExcuseBrowser();
+				driver.Manage().Window.Size = new Size(1366,680);
+				driver.Navigate().GoToUrl("https://freenanofaucet.com/");
+				//Đợi đến khi website load xong hết các đoạn javascript:
+				WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60.0));
+				wait.Until((x) => { return ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"); });
+				string wallet = ""; string result = "";
+				Invoke((MethodInvoker)delegate
+				{
+					wallet = TXT_wallet.Text.Trim();
+				});
+				if (isStop) goto closeall; 
+				Thread.Sleep(2000);
+				driver.FindElement(By.Id("nanoAddr")).SendKeys(wallet);
+				if (isStop) goto closeall;
+				Invoke((MethodInvoker)delegate
+				{
+					TXT_Logs.AppendText("Nhập địa chỉ ví nano \r\n");
+				});
+				Thread.Sleep(1000);
+				if (isStop) goto closeall;
+				driver.FindElement(By.Id("getNano")).Click();
+				Thread.Sleep(2000);
+				result = driver.FindElement(By.XPath("//*[@id=\"body-body-wrap\"]/div/p")).Text;
+				if (isStop) goto closeall;
+				Invoke((MethodInvoker)delegate
+				{
+					TXT_Logs.AppendText("Thông báo:" + result  + "\r\n");
+				});
+
+				closeall:
+				driver.Close(); driver.Quit();
+			}
+            catch (Exception ex)
+            {
+				WriteFile(ex.ToString(), "error.txt");
+			}
+			
+		}
+		private void WriteFile(string data_text, string filepath)
+		{
+			using (StreamWriter sw = new StreamWriter(filepath, true))//Append thì true, ghi đè thì false
+			{
+				sw.WriteLine(data_text);
+			}
+		}
+		private void BTN_Start_Click(object sender, EventArgs e)
+        {
+			if (BTN_Start.Text == "START")
+			{
+				isStop = false;
+				BTN_Start.Text = "STOP";
+				Thread thread = new Thread((ThreadStart)delegate
+				{
+					Action();
+				});
+				thread.IsBackground = true;
+				thread.Start();
+			}
+			else
+			{
+				isStop = true;
+				BTN_Start.Text = "START";
+			}
+		}
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+			int hh = DateTime.Now.Hour;
+			int mm = DateTime.Now.Minute;
+			int ss = DateTime.Now.Second;
+			if ((mm%10 == 0) & (ss == 01))
+			{
+				Invoke((MethodInvoker)delegate ()
+				{
+					TXT_Logs.AppendText("Tự khởi động lại tool sau mỗi 10 phút \r\n");
+				});
+				if (BTN_Start.Text == "STOP")
+				{
+					isStop = false;
+					BTN_Start.Text = "START";
+				}
+				BTN_Start_Click(sender, e);
+			}
+			LBL_Time.Text = "Time: " + hh + ":" + mm + ":" + ss;
+		}
+
+        private void LinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+			Process.Start(LinkLabel1.Text);
+        }
+    }
+}
